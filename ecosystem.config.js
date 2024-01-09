2c955046260e615d26cb94d111e92c1a69383f81
dibya-sunstone <dibya.acharya@sunstone.edu.in>
2024-01-09 16:40:25 +0530

    openssl for phantom js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,9 +7,11 @@ module.exports = {
             watch: process.env.NODE_ENV === 'development',
             env: {
                 NODE_ENV: 'development',
+                OPENSSL_CONF: '/dev/null'
             },
             env_production: {
                 NODE_ENV: 'production',
+                OPENSSL_CONF: '/dev/null',
                 WATCH: false
             },
         },
